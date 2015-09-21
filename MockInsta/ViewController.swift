66326aa0c088b11b1e0357ffb68161b558d7b464
adityavchaturvedi <aditya2013@berkeley.edu>
2015-09-20 19:47:41 -0700

    Beginning the process now

---
+++ MockInsta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MockInsta
+//
+//  Created by Aditya Chaturvedi on 9/20/15.
+//  Copyright © 2015 Aditya V Chaturvedi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MockInstaTests/MockInstaTests.swift
@@ -0,0 +1,36 @@
+//
+//  MockInstaTests.swift
+//  MockInstaTests
+//
+//  Created by Aditya Chaturvedi on 9/20/15.
+//  Copyright © 2015 Aditya V Chaturvedi. All rights reserved.
+//
+
+import XCTest
+@testable import MockInsta
+
+class MockInstaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
