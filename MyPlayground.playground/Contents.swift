14a83ccd24b29ccb0d84a22f8529c88a4bc3bf24
adityavchaturvedi <aditya2013@berkeley.edu>
2015-09-20 20:58:31 -0700

    Added the Pod File

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import Alamofire
+import XCPlayground
+
+XCPExecutionShouldContinueIndefinitely(true)
+
+Alamofire.request(.GET, "https://httpbin.org/get").responseJSON {_,_,result in print(result)
+    debugPrint(result)
+}
+
